008ce3112d44cdf84e924011e1dbfd8ccab37e40
Dimitris Baltas <dbaltas@travelplanet24.com>
2013-05-16 14:17:41 +0300

    setup selenium server as a service

---
+++ setup/selenium-server.sh
@@ -0,0 +1,4 @@
+sudo curl http://selenium.googlecode.com/files/selenium-server-standalone-2.32.0.jar --create-dirs -o /usr/lib/selenium-server/selenium-server-standalone-2.32.0.jar &&
+sudo cp init.d-script /etc/init.d/selenium_server && 
+sudo mkdir /var/log/selenium-server
+
